d9938feab266bc4cbef4c4c8b4fa2541be000e87
Juan García <juangarcia@mailinator.com>
2013-03-21 12:41:37 +0000

    OnClickListener

--- src/com/artemisa/yamba/StatusActivity.java
+++ src/com/artemisa/yamba/StatusActivity.java
@@ -1,23 +1,41 @@
 package com.artemisa.yamba;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener {
+	private static final String TAG = "StatusActivity";
+	private Button updateButton;
+	private TextView editText;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_status);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
 
+		// Find views
+		updateButton = (Button) findViewById(R.id.buttonUpdate);
+		updateButton.setOnClickListener(this);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.status, menu);
-        return true;
-    }
-    
+		editText = (TextView) findViewById(R.id.editText);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.status, menu);
+		return true;
+	}
+
+	// Called when button is clicked
+	public void onClick(View v) {
+		Log.d(TAG, "OnCLick: " + editText.getText().toString());
+	}
+
 }
