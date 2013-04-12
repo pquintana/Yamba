5a950c1944fe4d66314e95161d8cce4ad75c0ff6
Juan García <juangarcia@mailinator.com>
2013-04-12 17:55:20 +0100

    Asynchronous Threads

--- src/com/artemisa/yamba/StatusActivity.java
+++ src/com/artemisa/yamba/StatusActivity.java
@@ -3,6 +3,7 @@ package com.artemisa.yamba;
 import winterwell.jtwitter.Twitter;
 import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -10,14 +11,45 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class StatusActivity extends Activity implements OnClickListener {
 	private static final String TAG = "StatusActivity";
 	private Button updateButton;
 	private TextView editText;
-	
 	private Twitter twitter;
 
+	//asynchronously posts to twitter
+	
+	class PostToTwitter extends AsyncTask<String, Integer, String>{
+	
+	@Override
+	
+	protected String doInBackground(String...statuses) {
+		try {
+		  Twitter.Status status = twitter.updateStatus(statuses[0]);
+		  return status.text;
+		}catch (TwitterException e) {
+		  Log.e(TAG, e.toString());
+		  e.printStackTrace();
+		  return "Failed to post";
+		}
+	}
+	
+	@Override
+	protected void onProgressUpdate(Integer... values){
+		super.onProgressUpdate(values);
+	}
+	
+	@Override
+	
+	protected void onPostExecute(String result) {
+		Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+	}
+	
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -42,13 +74,16 @@ public class StatusActivity extends Activity implements OnClickListener {
 
 	// Called when button is clicked
 	public void onClick(View v) {
-	  try{
-		twitter.setStatus(editText.getText().toString());
-		Log.d(TAG, "OnCLick: " + editText.getText().toString());
-	  } catch (TwitterException e) {
-		  Log.e(TAG, e.toString());
-		  e.printStackTrace();
-	  }
+	// try{
+	//	twitter.setStatus(editText.getText().toString());
+	//	Log.d(TAG, "OnCLick: " + editText.getText().toString());
+	//  } catch (TwitterException e) {
+	//	  Log.e(TAG, e.toString());
+	//	  e.printStackTrace();
+	//  }
+		
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status);
 	}
 
 }
